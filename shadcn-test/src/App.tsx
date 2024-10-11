94437db80ba4b3b602c24b1ea320ae9c2d5e3cb6
Adria Blancafort <adriablancafort@gmail.com>
2024-10-11 12:27:15 +0200

    add demo components

--- shadcn-test/src/App.tsx
+++ shadcn-test/src/App.tsx
@@ -1,7 +1,13 @@
-export default function App() {
+import { DatePicker } from "@/components/DatePicker.tsx"
+import { CardWithForm } from "@/components/CardWithForm.tsx"
+import { Chart } from "@/components/Chart.tsx"
+
+export default function Home() {
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <div>
+      <DatePicker />
+      <CardWithForm />
+      <Chart />
+    </div>
   )
 }
