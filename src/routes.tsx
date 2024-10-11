fba18af9721de506b657d8dd3ca075fe4a178e5a
Adria Blancafort <adriablancafort@gmail.com>
2024-10-11 17:50:07 +0200

    feat: add code splitting

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
-import Email from '@/pages/Email';
+import { Suspense, lazy } from 'react';
+import { LoaderCircle } from 'lucide-react';
+
+const Home = lazy(() => import('@/pages/Home'));
+const Email = lazy(() => import('@/pages/Email'));
 
 function AppRoutes() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/email" element={<Email />} />
-      </Routes>
+      <Suspense fallback={
+        <div className="flex items-center justify-center h-screen">
+          <LoaderCircle className="animate-spin" size={48} />
+        </div>
+      }>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/email" element={<Email />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
